278889c62878b2ecf89f8c1345068d9b4e0141a5
James Morris <70540466+jleomorris@users.noreply.github.com>
2022-04-03 12:12:35 +0100

    Update server file

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const { ApolloServer, gql } = require("apollo-server");
-const { books } = require("./data");
+const { mainCards, products, categories } = require("./data");
 const typeDefs = require("./schema");
 // Resolvers
+// Resolvers
 const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const Category = require("./resolvers/Category");
+const Product = require("./resolvers/Product");
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
   cors: true,
   typeDefs,
-  resolvers: { Query },
+  resolvers: { Query, Mutation, Category, Product },
+  context: {
+    mainCards,
+    products,
+    categories,
+  },
   introspection: true,
-  context: { books },
 });
 
 // The `listen` method launches a web server.
